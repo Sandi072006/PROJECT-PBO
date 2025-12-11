1e763572441885d118fcc79248ddb4b779180583
putri <putri.fauziah206@gmail.com>
2025-12-12 03:44:25 +0700

    Update App.java

--- Penilaian_Mahasiswa/src/main/java/com/mycompany/penilaian_mahasiswa/App.java
+++ Penilaian_Mahasiswa/src/main/java/com/mycompany/penilaian_mahasiswa/App.java
@@ -3,22 +3,25 @@ package com.mycompany.penilaian_mahasiswa;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
+import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/mycompany/penilaian_mahasiswa/primary.fxml"));
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("/com/mycompany/penilaian_mahasiswa/login.fxml")
+        );
         Parent root = loader.load();
         
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 600, 700);
         stage.setScene(scene);
-        stage.setTitle("Manajemen Nilai Akademik");
+        stage.setTitle("Login - Sistem Penilaian Mahasiswa");
+        stage.setResizable(false);
         stage.show();
     }
+    
     public static void main(String[] args) {
         launch(args);
     }
